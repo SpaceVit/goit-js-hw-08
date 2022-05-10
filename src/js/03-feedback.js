46523c97a402805c0ff4e1b041496bdd358c48bb
Vitaliy Pavlovskiy <sledozar@gmail.com>
2022-05-10 08:04:17 +0300

    finish-all-tasks

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
+import throttle from 'lodash.throttle';
 
+const formEl = document.querySelector('form');
+const inputEl = document.querySelector("input[name = 'email']");
+const textareaEl = document.querySelector("textarea[name = 'message']");
+const STORAGE_KEY = 'feedback-form-state';
+const userData = {};
+
+formEl.addEventListener('input', throttle(setDataToStorage, 500));
+formEl.addEventListener('submit', actionsOnSubmit);
+
+getDataFromStorage();
+
+function setDataToStorage(evt) {
+  userData[evt.target.name] = evt.target.value;
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(userData));
+}
+
+function actionsOnSubmit(evt) {
+  evt.preventDefault();
+
+  if (inputEl.value === '' || textareaEl.value === '') {
+    return alert('My dear friend, please fill in all the fields!');
+  }
+  evt.target.reset();
+  localStorage.removeItem(STORAGE_KEY);
+  return alert('Thank you for your feedback, my dear friend');
+}
+
+function getDataFromStorage() {
+  const dataFromStorage = localStorage.getItem(STORAGE_KEY);
+  const parsedData = JSON.parse(dataFromStorage);
+
+  if (dataFromStorage) {
+    inputEl.value = parsedData.email;
+    textareaEl.value = parsedData.message;
+  }
+}
+// 1.отслеживаем весь текст и записываем в локалсторидж в виде обьекта при помощи стрингифай;
+// 2.при сабмите очищаем все поля формы и локалсторидж;
+// 3.при обновлении страницы записываем данные с локалсторидж в поля при помощи парс;
